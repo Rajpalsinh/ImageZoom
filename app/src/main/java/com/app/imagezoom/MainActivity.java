e9291e26a36601e4b885dd394471f18f8ee7c20a
RAj <rajpalsinh.shinora@bhavitechnologies.com>
2019-06-04 16:05:58 +0530

    commit

--- app/src/main/java/com/app/imagezoom/MainActivity.java
+++ app/src/main/java/com/app/imagezoom/MainActivity.java
@@ -33,7 +33,14 @@ public class MainActivity extends AppCompatActivity {
         mImages.addAll(Arrays.asList(R.drawable.img1, R.drawable.img2,
                 R.drawable.img3, R.drawable.img4, R.drawable.img5,
                 R.drawable.img6, R.drawable.img7, R.drawable.img8,
-                R.drawable.img9, R.drawable.img10));
+                R.drawable.img9, R.drawable.img10, R.drawable.img11,
+                R.drawable.img12, R.drawable.img13, R.drawable.img14,
+                R.drawable.img15, R.drawable.img16, R.drawable.img17,
+                R.drawable.img18, R.drawable.img19, R.drawable.img20,
+                R.drawable.img21, R.drawable.img22, R.drawable.img23,
+                R.drawable.img24, R.drawable.img25, R.drawable.img26,
+                R.drawable.img27, R.drawable.img28
+        ));
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv);
 
